61603af36a0741bfbdc4530d86953073d847f55e
auralshin <anshulspartan141@gmail.com>
2020-01-19 05:13:18 +0530

    front page layout made

---
+++ js/custom.js
@@ -0,0 +1,20 @@
+
+$(function () {
+
+    // animate on scroll
+    new WOW().init();
+});
+
+
+$(function () {
+
+    $("#work").magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+
+    });
+});
+
