4881ee11e523cb38e28784fc4f91b3cc732c908d
auralshin <anshulspartan141@gmail.com>
2021-07-06 15:34:09 +0530

    changes : - App.js : event page removed - Home.js : event component added

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import React, { Fragment } from 'react';
 import Home from './pages/home/Home';
-import Events from './pages/events/Events';
+
 const App = () => {
   return (
     <Fragment>
       <main>
         <Home />
-        <Events />
+        
       </main>
     </Fragment>
   );
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Landing from '../../components/LandingPage/landing';
 import Nav from '../../components/Nav/Nav';
 import About from '../../components/Aboutus/About';
+import Events from './components/Events/Event';
 const Home = () => {
   return (
   <div>
     <Nav />
    <Landing />
    <About />
+   <Events/>
   </div>
   )
 };
