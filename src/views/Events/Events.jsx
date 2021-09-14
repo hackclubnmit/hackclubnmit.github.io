88a1aafac3ac7d30de2d994de567c2a922eab207
aastha432 <aasthasarkar13gul@gmail.com>
2021-09-15 00:23:26 +0530

    Events page updated

--- src/views/Events/Events.jsx
+++ src/views/Events/Events.jsx
@@ -10,15 +10,88 @@ import {
   Button,
   CardTitle,
   CardText,
-  Row,
-  Col,
   Container,
   CardImg,
   CardBody,
   CardSubtitle,
 } from "reactstrap";
 import classnames from "classnames";
+import Carousel from 'react-material-ui-carousel'
+import { Paper } from '@material-ui/core'
 
+function CarouselCard(props)
+{
+    var items = [
+        {
+            name: "Random Name #1",
+            description: "Probably the most random thing you have ever seen!"
+        },
+        {
+            name: "Random Name #2",
+            description: "Hello World!"
+        },
+        {
+          name: "Random Name #3",
+          description: "Hello World!"
+        },
+        {
+          name: "Random Name #4",
+          description: "Hello World!"
+        },
+        {
+          name: "Random Name #5",
+          description: "Hello World!"
+        },
+        {
+          name: "Random Name #6",
+          description: "Hello World!"
+        },
+        {
+          name: "Random Name #7",
+          description: "Hello World!"
+        }
+        
+    ]
+
+    return (
+        <Carousel animation="slide">
+            {
+                items.map( (item, i) => <Item key={i} item={item} /> )
+            }
+        </Carousel>
+    )
+}
+
+function Item(props)
+{
+    return (
+        <Paper elevation={3}>
+            <Card body color="dark">
+                  <CardImg
+                    top
+                    width="100%"
+                    src="https://images.unsplash.com/photo-1589652717521-10c0d092dea9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+                    alt="Card image cap"
+                  />
+                  <CardBody>
+                    <CardTitle tag="h5">Card title</CardTitle>
+                    <CardSubtitle tag="h6" className="mb-2 text-muted">
+                    {props.item.name}
+                    </CardSubtitle>
+                    <CardText>
+                    {props.item.description}
+                    </CardText>
+                    <Button>Check it out!</Button>
+                  </CardBody>
+                </Card>
+        </Paper>
+    )
+}
+
+
+
+
+ 
 const Events = () => {
   const [activeTab, setActiveTab] = useState("1");
 
@@ -39,7 +112,7 @@ const Events = () => {
                 toggle("1");
               }}
             >
-              All
+              Past
             </NavLink>
           </NavItem>
 
@@ -50,7 +123,7 @@ const Events = () => {
                 toggle("2");
               }}
             >
-              Upcoming
+              Current
             </NavLink>
           </NavItem>
 
@@ -61,199 +134,27 @@ const Events = () => {
                 toggle("3");
               }}
             >
-              Past
+              Upcoming
             </NavLink>
           </NavItem>
         </Nav>
 
         <TabContent activeTab={activeTab}>
           <TabPane tabId="1">
-            <Row>
-              <Col sm="4">
-                <Card body color="dark">
-                  <CardImg
-                    top
-                    width="100%"
-                    src="https://images.unsplash.com/photo-1517430816045-df4b7de11d1d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80"
-                    alt="Card image cap"
-                  />
-                  <CardBody>
-                    <CardTitle tag="h5">Card title</CardTitle>
-                    <CardSubtitle tag="h6" className="mb-2 text-muted">
-                      Card subtitle
-                    </CardSubtitle>
-                    <CardText>
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </CardText>
-                    <Button>Button</Button>
-                  </CardBody>
-                </Card>
-              </Col>
-
-              <Col sm="4">
-                <Card body color="dark">
-                  <CardImg
-                    top
-                    width="100%"
-                    src="https://images.unsplash.com/photo-1517430816045-df4b7de11d1d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80"
-                    alt="Card image cap"
-                  />
-                  <CardBody>
-                    <CardTitle tag="h5">Card title</CardTitle>
-                    <CardSubtitle tag="h6" className="mb-2 text-muted">
-                      Card subtitle
-                    </CardSubtitle>
-                    <CardText>
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </CardText>
-                    <Button>Button</Button>
-                  </CardBody>
-                </Card>
-              </Col>
-
-              <Col sm="4">
-                <Card body color="dark">
-                  <CardImg
-                    top
-                    width="100%"
-                    src="https://images.unsplash.com/photo-1517430816045-df4b7de11d1d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80"
-                    alt="Card image cap"
-                  />
-                  <CardBody>
-                    <CardTitle tag="h5">Card title</CardTitle>
-                    <CardSubtitle tag="h6" className="mb-2 text-muted">
-                      Card subtitle
-                    </CardSubtitle>
-                    <CardText>
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </CardText>
-                    <Button>Button</Button>
-                  </CardBody>
-                </Card>
-              </Col>
-            </Row>
+              <h4>List of past events (This heading will be deleted after API integration)</h4>
+              <CarouselCard/>
           </TabPane>
 
           <TabPane tabId="2">
-            <Row>
-              <Col sm="6">
-                <Card body color="dark">
-                  <CardImg
-                    top
-                    width="100%"
-                    src="https://images.unsplash.com/photo-1517430816045-df4b7de11d1d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80"
-                    alt="Card image cap"
-                  />
-                  <CardBody>
-                    <CardTitle tag="h5">Card title</CardTitle>
-                    <CardSubtitle tag="h6" className="mb-2 text-muted">
-                      Card subtitle
-                    </CardSubtitle>
-                    <CardText>
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </CardText>
-                    <Button>Button</Button>
-                  </CardBody>
-                </Card>
-              </Col>
-
-              <Col sm="6">
-                <Card body color="dark">
-                  <CardImg
-                    top
-                    width="100%"
-                    src="https://images.unsplash.com/photo-1517430816045-df4b7de11d1d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80"
-                    alt="Card image cap"
-                  />
-                  <CardBody>
-                    <CardTitle tag="h5">Card title</CardTitle>
-                    <CardSubtitle tag="h6" className="mb-2 text-muted">
-                      Card subtitle
-                    </CardSubtitle>
-                    <CardText>
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </CardText>
-                    <Button>Button</Button>
-                  </CardBody>
-                </Card>
-              </Col>
-            </Row>
+          <h4>List of current events (This heading will be deleted after API integration)</h4>
+          <CarouselCard/>
           </TabPane>
 
           <TabPane tabId="3">
-            <Row>
-              <Col sm="4">
-                <Card body color="dark">
-                  <CardImg
-                    top
-                    width="100%"
-                    src="https://images.unsplash.com/photo-1589652717521-10c0d092dea9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
-                    alt="Card image cap"
-                  />
-                  <CardBody>
-                    <CardTitle tag="h5">Card title</CardTitle>
-                    <CardSubtitle tag="h6" className="mb-2 text-muted">
-                      Card subtitle
-                    </CardSubtitle>
-                    <CardText>
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </CardText>
-                    <Button>Button</Button>
-                  </CardBody>
-                </Card>
-              </Col>
-
-              <Col sm="4">
-                <Card body color="dark">
-                  <CardImg
-                    top
-                    width="100%"
-                    src="https://images.unsplash.com/photo-1589652717521-10c0d092dea9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
-                    alt="Card image cap"
-                  />
-                  <CardBody>
-                    <CardTitle tag="h5">Card title</CardTitle>
-                    <CardSubtitle tag="h6" className="mb-2 text-muted">
-                      Card subtitle
-                    </CardSubtitle>
-                    <CardText>
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </CardText>
-                    <Button>Button</Button>
-                  </CardBody>
-                </Card>
-              </Col>
-
-              <Col sm="4">
-                <Card body color="dark">
-                  <CardImg
-                    top
-                    width="100%"
-                    src="https://images.unsplash.com/photo-1589652717521-10c0d092dea9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
-                    alt="Card image cap"
-                  />
-                  <CardBody>
-                    <CardTitle tag="h5">Card title</CardTitle>
-                    <CardSubtitle tag="h6" className="mb-2 text-muted">
-                      Card subtitle
-                    </CardSubtitle>
-                    <CardText>
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </CardText>
-                    <Button>Button</Button>
-                  </CardBody>
-                </Card>
-              </Col>
-            </Row>
+          <h4>List of upcoming events (This heading will be deleted after API integration)</h4>
+          <CarouselCard/>
           </TabPane>
+
         </TabContent>
       </Container>
     </div>
